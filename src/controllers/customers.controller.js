83169f9d7c1499ca07da72987dd7f6199bdffc2e
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2023-05-14 16:41:59 -0300

    fix: customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -3,7 +3,7 @@ import { db } from '../database/database.js'
 export async function getCustomers(req, res) {
   try {
     const { rows } = await db.query('SELECT * FROM customers;')
-    res.status(200).send(rows[0])
+    res.status(200).send(rows)
   } catch (err) {
     res.sendStatus(500)
   }
@@ -27,7 +27,7 @@ export async function createCustomers(req, res) {
 export async function getCustomerId(req, res) {
   const customer = res.locals.customer
   try {
-    res.send(customer)
+    res.send(customer[0])
   } catch (err) {
     res.sendStatus(500)
   }
@@ -44,7 +44,7 @@ export async function updateCustomer(req, res) {
       `UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`,
       [name, phone, cpf, birthday, id]
     )
-    res.sendStatus(201)
+    res.sendStatus(200)
   } catch (err) {
     res.sendStatus(500)
   }
--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -2,7 +2,16 @@ import joi from 'joi'
 
 export const createCustomersSchema = joi.object({
   name: joi.string().min(3).required(),
-  phone: joi.string().min(10).max(11).required(),
-  cpf: joi.string().length(11).required(),
+  phone: joi
+    .string()
+    .min(10)
+    .max(11)
+    .pattern(/^[0-9]+$/)
+    .required(),
+  cpf: joi
+    .string()
+    .min(11)
+    .pattern(/^[0-9]+$/)
+    .required(),
   birthday: joi.date().required()
 })
